29c86876710cd858dc4bff72fbd8877eab4c4c12
davidhenley <henleydr@gmail.com>
2017-02-08 14:56:19 -0600

    Create LinkCreate component

--- client/main.js
+++ client/main.js
@@ -2,11 +2,13 @@ import React from 'react';
 import { render } from 'react-dom';
 
 import Header from './components/header';
+import LinkCreate from './components/link_create';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <LinkCreate />
     </div>
   );
 };
