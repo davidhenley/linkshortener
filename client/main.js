bf0efb317daff72bfd9f5dff384977a2ddc1447f
davidhenley <henleydr@gmail.com>
2017-02-09 12:21:19 -0600

    Create LinkList component

--- client/main.js
+++ client/main.js
@@ -5,12 +5,14 @@ import { Links } from '../imports/collections/links';
 
 import Header from './components/header';
 import LinkCreate from './components/link_create';
+import LinkList from './components/link_list';
 
 const App = () => {
   return (
     <div className="app">
       <Header />
       <LinkCreate />
+      <LinkList />
     </div>
   );
 };
--- server/main.js
+++ server/main.js
@@ -3,5 +3,7 @@ import { Meteor } from 'meteor/meteor';
 import { Links } from '../imports/collections/links';
 
 Meteor.startup(() => {
-
+  Meteor.publish('links', function() {
+    return Links.find({});
+  });
 });
